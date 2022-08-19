de029a34a4f2072031c73e78ddafe73f65921c5c
Bhaskar <98847406+happy4soul@users.noreply.github.com>
2022-08-19 16:26:03 +0530

    Initial commit

---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li> Boruto</li>
+      <li> Boruto</li>
+      <li> Boruto</li>
+      <li> Boruto</li>
+    </ul>
+  );
+}
+
+export default List;
